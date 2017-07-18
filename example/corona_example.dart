ddcbe4760af42f7159bc84127d1ffc00fb1633a8
Igindo\FrankPepermans <frank@igindo.com>
2017-07-18 11:48:41 +0200

    rem example folder

--- example/corona_example.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2017, Frank. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-import 'package:corona/corona.dart';
-
-main() {
-}
